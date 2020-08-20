9c8824fddc3aa3ef64b9aa3b3d9f03aaa43d8d42
Dmitry Brant <dbrant@wikimedia.org>
2020-08-19 22:59:49 -0400

    Remove unused header.

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -1,5 +1,16 @@
-#include "framework.h"
-#include "mandelbrot.h"
+#include "targetver.h"
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files
+#include <windows.h>
+#include <commctrl.h>
+
+// C RunTime Header Files
+//#include <stdlib.h>
+//#include <malloc.h>
+//#include <memory.h>
+#include <tchar.h>
+
+#include "resource.h"
 
 extern "C"
 {
