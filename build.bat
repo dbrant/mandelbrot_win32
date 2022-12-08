98eb9b8e9449348df5c2a95f3e227aeb9eaac4d4
Dmitry Brant <me@dmitrybrant.com>
2022-12-07 19:30:32 -0500

    Don't generate pdb.

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 
 rc mandelbrot.rc
 
-cl.exe -nologo /D "NDEBUG" /D "NDEBUG" /D "_WINDOWS" /D "_UNICODE" /D "UNICODE" /GL /W3 /Zc:wchar_t /fp:fast /Zi /Gm- /O2 /GR- /EHa- /Oi /Ot /GS- /arch:AVX2 /Gd /Oi /MT /FC mandelbrot.cpp mandelbrot.res /link /nodefaultlib user32.lib kernel32.lib gdi32.lib
+cl.exe -nologo /D "NDEBUG" /D "NDEBUG" /D "_WINDOWS" /D "_UNICODE" /D "UNICODE" /GL /W3 /Zc:wchar_t /fp:fast /Gm- /O2 /GR- /EHa- /Oi /Ot /GS- /arch:AVX2 /Gd /Oi /MT /FC mandelbrot.cpp mandelbrot.res /link /nodefaultlib user32.lib kernel32.lib gdi32.lib
